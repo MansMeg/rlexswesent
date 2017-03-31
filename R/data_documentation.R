0bffb3ad7ae8af6c9366e0201075b6b3b50dd9c4
Måns Magnusson <mons.magnusson@gmail.com>
2017-03-31 13:43:38 +0200

    Init package

---
+++ R/data_documentation.R
@@ -0,0 +1,7 @@
+#' Sentiment lexicon for Swedish
+#'
+#' @format A \code{tbl_df} with one row per lemgram.
+#' 
+#' @references See Språkbankens resources.
+#' 
+"swesent"
---
+++ R/rlexswesent.R
@@ -0,0 +1,10 @@
+#' rlexswesent R package
+#'
+#' @description 
+#' An convinience R package that contain the Swedish sentiment lexicon from Språkbanken, 
+#' together with functionality to use with tidytext formats and dplyr.
+#'
+#' @docType package
+#' @name rlexswesent-package
+#' @aliases rlexswesent
+NULL
